17562514c4d0e34b267c1eb0625f3a203167d2ff
Jim Neville <jrneville@gmail.com>
2020-01-31 23:18:30 -0700

    add venv and first test

---
+++ tests/test_player_draw.py
@@ -0,0 +1,19 @@
+import unittest
+
+from blackjack import Player, Deck#, Card
+
+class TestDraw(unittest.TestCase):
+	def test_draw(self):
+		p1 = Player()
+		deck = Deck()
+		p1.draw(deck)
+
+		result = len(p1.hand)
+
+		self.assertEqual(result, 1)
+
+if __name__ == "__main__":
+	unittest.main()
+
+
+
